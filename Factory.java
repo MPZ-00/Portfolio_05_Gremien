1f9483007592362958a54987e1ab82e920e71e92
Martin <martin.praetzlich@outlook.de>
2022-12-12 18:00:06 +0100

    +Objekte hinzufügen

--- Factory.java
+++ Factory.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Factory {
-    private HashMap<String, HashSet<Integer>> objects;
+    private HashMap<String, HashSet<Integer>> objects = new HashMap<>();
     private static Factory instance;
 
     private Factory() {}
+
     public static Factory getInstance() {
         if (instance == null) {
             instance = new Factory();
@@ -15,20 +16,43 @@ public class Factory {
         return instance;
     }
 
+    public void addID(String className, Integer id) {
+        if (!objects.containsKey(className)) {
+            objects.put(className, new HashSet<Integer>());
+        }
+
+        objects.get(className).add(id);
+    }
+
     public Gremien createGremien(String Name, Boolean offiziell, Boolean inoffiziell, LocalDate Beginn, LocalDate Ende) {
-        return new Gremien(getGremienID(), Name, offiziell, inoffiziell, Beginn, Ende);
+        Integer id = getGremienID();
+        Gremien new_Gremien = new Gremien(id, Name, offiziell, inoffiziell, Beginn, Ende);
+        addID(Gremien.class.toString(), id);
+        return new_Gremien;
     }
     public Antrag createAngtrag(String Titel, String Text, IAntrag.Ergebnis Ergebnis, boolean Angenommen) {
-        return new Antrag(getAntragID(), Titel, Text, Ergebnis, Angenommen);
+        Integer id = getAntragID();
+        addID(Antrag.class.toString(), id);
+        Antrag new_Antrag = new Antrag(id, Titel, Text, Ergebnis, Angenommen);
+        return new_Antrag;
     }
     public Tagesordnung createTagesordnung(String Titel, String Kurzbeschreibung, String Protokolltext) {
-        return new Tagesordnung(getTagesordnungID(), Titel, Kurzbeschreibung, Protokolltext);
+        Integer id = getTagesordnungID();
+        addID(Tagesordnung.class.toString(), id);
+        Tagesordnung new_Tagesordnung = new Tagesordnung(id, Titel, Kurzbeschreibung, Protokolltext);
+        return new_Tagesordnung;
     }
     public Aufgabengebiete createAufgabengebiete(int Ag_ID, String Aufgabengebiet) {
-        return new Aufgabengebiete(getAufgabengebieteID(), Ag_ID, Aufgabengebiet);
+        Integer id = getAufgabengebieteID();
+        addID(Aufgabengebiete.class.toString(), id);
+        Aufgabengebiete new_Aufgabengebiete = new Aufgabengebiete(id, Ag_ID, Aufgabengebiet);
+        return new_Aufgabengebiete;
     }
     public Sitzungen createSitzungen(Timestamp Beginn, Timestamp Ende, LocalDate Einladung_am, Boolean Oeffentlich, String Ort, String Protokoll) {
-        return new Sitzungen(getSitzungenID(), Beginn, Ende, Einladung_am, Oeffentlich, Ort, Protokoll);
+        Integer id = getSitzungenID();
+        addID(Sitzungen.class.toString(), id);
+        Sitzungen new_Sitzungen = new Sitzungen(getSitzungenID(), Beginn, Ende, Einladung_am, Oeffentlich, Ort, Protokoll);
+        return new_Sitzungen;
     }
     
     private Integer getGremienID() {
