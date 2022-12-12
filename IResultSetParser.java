8936ec46ecd10aa6c2f9360634436065a1575d5b
Martin <martin.praetzlich@outlook.de>
2022-12-12 19:52:08 +0100

    - unnötige Klasse

--- IResultSetParser.java
+++
@@ -1,8 +0,0 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface IResultSetParser {
-    public List<Aufgabengebiete> getAufgabengebieteFromResultSet(ResultSet rs) throws SQLException;
-    public List<Tagesordnung> getTagesordnungenFromResultSet(ResultSet rs) throws SQLException;
-}
